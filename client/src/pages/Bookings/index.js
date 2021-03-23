700ec6f0edc4aef16974f4b5cf8cb0f2be8e331a
Tiago <tiagolpires@hotmail.com>
2021-03-23 11:12:37 -0300

    Added Bookings Page

--- client/src/pages/Bookings/index.js
+++ client/src/pages/Bookings/index.js
@@ -1,11 +1,38 @@
+import Header from '../../components/Header'
 import './style.css'
 
-const index = ({ teste }) => {
+const index = () => {
     return (
-        <div>
-            <h1>Bookings</h1>
-            <h1>{teste}</h1>
-        </div>
+        <>
+            <Header/>
+            <section className="bookings-section">
+                <h1>Your Bookings</h1>
+                <div className="bookings-container">
+                    <div className="booking-ticket">
+                        <div className="ticket-seat">
+                            <h2>A01</h2>
+                            <div className="ticket-division"></div>
+                        </div>
+                        <div className="booking-info">
+                            <h3>Once Upon a time in Hollywood</h3>
+                            <span><b>Name:</b> Júlia</span>
+                            <span><b>Seat:</b> A01</span>
+                        </div>
+                    </div>
+                    <div className="booking-ticket">
+                        <div className="ticket-seat">
+                            <h2>A01</h2>
+                            <div className="ticket-division"></div>
+                        </div>
+                        <div className="booking-info">
+                            <h3>Once Upon a time in Hollywood</h3>
+                            <span><b>Name:</b> Júlia</span>
+                            <span><b>Seat:</b> A01</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
